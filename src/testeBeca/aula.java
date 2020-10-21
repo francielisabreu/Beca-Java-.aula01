33bcd7e1e345056bc06f9b56a4a86c83b10bb1fc
Francieli Silva Abreu <fsilvaab@everis.com>
2020-10-21 13:35:10 -0300

    laços de repetição

--- src/testeBeca/aula.java
+++ src/testeBeca/aula.java
@@ -3,6 +3,51 @@ package testeBeca;
 public class aula {
 
 	public static void main(String[] args) {
-		System.out.println("ola mundo");
+		//System.out.println("Olá mundo");
+		
+		
+//		boolean valor = false;
+//		
+//		if(valor == true) {
+//			System.out.println("Está logica é verdadeira");
+//			
+//		} else {
+//			System.out.println("Está logica é falsa");
+//		}
+		
+		
+		
+//		for(int i = 0; i < 100; ++i) {
+//			
+//			System.out.println(i);
+//		}
+		
+		
+		
+//		int diaDaSemana = 4;
+//		String nomeDoDia;
+//		
+//		switch(diaDaSemana) {
+//		case 1:nomeDoDia = "Domingo"; break;
+//		case 2:nomeDoDia = "Segunda"; break;
+//		case 3:nomeDoDia = "Terça"; break;
+//		case 4:nomeDoDia = "Quarta"; break;
+//		case 5:nomeDoDia = "Quinta"; break;
+//		case 6:nomeDoDia = "Sexta"; break;
+//		case 7:nomeDoDia = "Sábado"; break;
+//		default:nomeDoDia = "Dia inválido"; break; 
+//			
+//		}
+//		System.out.println(nomeDoDia);	
+		
+		
+//		int x = 1;
+//		while(x < 11) {
+//			System.out.println(x);
+//			++x;
+//		}
+		
+		
 	}
 }
+
